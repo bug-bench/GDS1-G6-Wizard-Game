b6d8077c5ad8de8a47831805e859de410783ef95
MitchPlays <Mashton2005@icloud.com>
2026-03-26 12:41:07 +1100

    partial completion of US12 and completion of US13 & US16
    added a scene transition between phase 1 and 2, with dummy code for saving Player Data across scenes

--- Assets/Scripts/PersistentObject.cs
+++ Assets/Scripts/PersistentObject.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class PersistentObject : MonoBehaviour
 {
+    public static PersistentObject Instance;
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 }
--- Assets/Scripts/Phase1Script.cs
+++ Assets/Scripts/Phase1Script.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Phase1Script : MonoBehaviour
 {
@@ -31,5 +33,30 @@ public class Phase1Script : MonoBehaviour
     {
         Debug.Log("Phase 1 Complete!");
         //add code to transfer data and players to phase 2
+        StartCoroutine(PhaseTransition());
+    }
+
+    void TransferPlayerDataToNextScene()
+    {
+        Debug.Log("Transferring player data to Phase 2...");
+
+        //Dummy logic for 4 players
+        //if adding more players or playing with less, change this to a foreach (Player) function
+        for (int i = 0; i < 4; i++)
+        {
+            Debug.Log("Saving data for Player " + (i + 1));
+        }
+    }
+
+    IEnumerator PhaseTransition()
+    {
+        //wait 20 seconds before switching scenes
+        yield return new WaitForSeconds(20f);
+
+        //call dummy function to carry over player data
+        TransferPlayerDataToNextScene();
+
+        //Load Phase 2 scene
+        SceneManager.LoadScene(1);
     }
 }
