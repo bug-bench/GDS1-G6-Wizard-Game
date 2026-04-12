6300691453b9df7d684a9311a260113603860d05
GHUANG-GSTAR <gordon.g.huang@student.uts.edu.au>
2026-04-13 00:47:39 +1000

    US2-14 As a player, I want destructible objects (crates/chests) so exploration is rewarding US2-15 As a player, I want loot from chests so I can gain spikes of power
    created destroyable crate where stats and spells drop from it after destroyed.Adjusted Spellprojectile script on the ontrigger part

---
+++ Assets/Scripts/destroyableObject.cs
@@ -0,0 +1,89 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class destroyableObject : MonoBehaviour
+{
+    public float health = 20f;
+
+    public GameObject[] StatPrefabs;
+    public int mindrops = 2;
+    public int maxdrops = 5;
+
+
+
+    public GameObject[] SpellPrefabs;
+    [Range(0f,1f)]
+    public float SpelldropChance = 0.2f;
+
+    public float dropforce = 3f;
+
+    private Vector3 startPosition;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+
+        if(health <=0)
+        {
+            BreakObject();
+        }
+    }
+   
+    void BreakObject()
+    {
+        DropStats();
+        DropSpells();
+
+        Destroy(gameObject);
+
+    }
+
+    void DropStats()
+    {
+        if (StatPrefabs.Length == 0) return;
+
+        int dropCount = Random.Range(mindrops, maxdrops + 1);
+
+        for (int i = 0; i<dropCount; i++)
+        {
+            GameObject prefab = StatPrefabs[Random.Range(0, StatPrefabs.Length)];
+
+            GameObject drops = Instantiate(prefab, transform.position, Quaternion.identity);
+
+            Rigidbody2D rb = drops.GetComponent<Rigidbody2D>();
+
+            if(rb !=null)
+            {
+                Vector2 force = Random.insideUnitCircle * dropforce;
+                rb.AddForce(force, ForceMode2D.Impulse);
+            }
+        }
+
+    }
+    void DropSpells()
+    {
+        if (SpellPrefabs.Length == 0) return;
+
+        float roll = Random.value;
+
+        if(roll<= SpelldropChance)
+        {
+            GameObject prefab = StatPrefabs[Random.Range(0, SpellPrefabs.Length)];
+
+            GameObject drops = Instantiate(prefab, transform.position, Quaternion.identity);
+
+            Rigidbody2D rb = drops.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                Vector2 force = Random.insideUnitCircle * dropforce;
+                rb.AddForce(force, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+}
